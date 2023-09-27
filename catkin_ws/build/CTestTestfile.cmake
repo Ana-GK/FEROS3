f2b5580d164d0a36b5a2cafe91a1ce65b03d1249
ana <ana.g.kostanjevec@gmail.com>
2023-09-27 10:43:49 +0200

    Initial commit

---
+++ catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/student/FEROS2023/FEROS_3/catkin_ws/src
+# Build directory: /home/student/FEROS2023/FEROS_3/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("robot_2dof")
---
+++ catkin_ws/build/robot_2dof/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/student/FEROS2023/FEROS_3/catkin_ws/src/robot_2dof
+# Build directory: /home/student/FEROS2023/FEROS_3/catkin_ws/build/robot_2dof
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
